a9aca19ea46a74267b4181e0a35e228357bfa8ab
Lê Quang Anh Đà <96710834+ITlqad1699@users.noreply.github.com>
2024-04-15 23:14:20 +0700

    [Spring-security][bug-01] Update table name contact_messages -> loans

--- security-server/security-module/src/main/java/com/eazybytes/entity/model/Loans.java
+++ security-server/security-module/src/main/java/com/eazybytes/entity/model/Loans.java
@@ -9,31 +9,31 @@ import org.hibernate.annotations.GenericGenerator;
 import java.util.Date;
 
 @Entity
-@Table(name = "contact_messages")
+@Table(name = "loans")
 @Getter
 @Setter
 public class Loans extends DateAudit {
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
-    @GenericGenerator(name = "native", strategy = "native")
-    @Column(name = "loan_number")
-    private Integer loanNumber;
-
-    @Column(name = "customer_id")
-    private Integer customerId;
-
-    @Column(name = "start_dt")
-    private Date startDt;
-
-    @Column(name = "loan_type")
-    private String loanType;
-
-    @Column(name = "total_loan")
-    private Integer totalLoan;
-
-    @Column(name = "amount_paid")
-    private Integer amountPaid;
-
-    @Column(name = "outstanding_amount")
-    private Integer outstandingAmount;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+	@GenericGenerator(name = "native", strategy = "native")
+	@Column(name = "loan_number")
+	private Integer loanNumber;
+	
+	@Column(name = "customer_id")
+	private Integer customerId;
+	
+	@Column(name = "start_dt")
+	private Date startDt;
+	
+	@Column(name = "loan_type")
+	private String loanType;
+	
+	@Column(name = "total_loan")
+	private Integer totalLoan;
+	
+	@Column(name = "amount_paid")
+	private Integer amountPaid;
+	
+	@Column(name = "outstanding_amount")
+	private Integer outstandingAmount;
 }
